79f20013b2e4bf9765862e23906d13fe8e4277a4
Simonlwd <sploegh@hotmail.com>
2026-05-14 15:09:06 +0200

    refactor verplicht notes en observed at

--- src/Form/ObservationType.php
+++ src/Form/ObservationType.php
@@ -6,16 +6,33 @@ use App\Entity\Observation;
 use App\Entity\User;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class ObservationType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('notes')
+            ->add('observedAt', DateTimeType::class, [
+                'constraints' => [
+                    new NotBlank(
+                        message: 'Vul een datum in'
+                    ),
+                ],
+            ])
+            ->add('notes', TextareaType::class, [
+                'required' => false,
+                'constraints' => [
+                    new NotBlank(
+                        message: 'Voeg een beschrijving toe van wat je hebt gezien.'
+                    )
+                ],
+            ])
             ->add('locationName')
             ->add('imagePath', FileType::class, [
                 'label' => 'Foto',
